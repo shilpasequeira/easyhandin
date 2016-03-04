8d61e4a0b5e0185829d7f2f68b7ef624c2eac566
Shilpa Sequeira <mail@shilpasequeira.com>
2016-03-04 04:13:42 -0800

    Change dashboard/index to just dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :teams
   resources :assignments
   resources :courses
+  resources :users
 
   get 'welcome/index'
 
@@ -14,11 +15,10 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
-  resources :users
-
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  match 'dashboard' => 'dashboard#index', :as => :dashboard, via: [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
