6f461f2fc54f096f822cf8239e78cc95b9216177
MonkeySR1 <monkeySR1@outlook.com>
2016-03-03 17:58:37 -0800

    Application_layout: add application view and add helper function to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :require_login
+  before_action :require_login, :current_uri, :path, :current_controller, :current_action
 
   def index
+    render template: "welcome/index"
   end
 
   private
@@ -19,5 +20,22 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_uri
+    @current_uri ||= request.env['PATH_INFO']
+  end
+
+  def path
+    @path ||= Rails.application.routes.recognize_path(current_uri)
+  end
+
+
+  def current_controller
+    @current_controller ||= path[:controller]
+  end
+
+  def current_action
+    @current_action ||= path[:action]
   end
 end
