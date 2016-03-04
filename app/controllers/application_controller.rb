dc82a992c72cecb926bf28d992dc245ff4eedea3
MonkeySR1 <monkeySR1@outlook.com>
2016-03-03 18:31:26 -0800

    Login_template: add login template and use helper to render only login or full application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :require_login, :current_uri, :path, :current_controller, :current_action
+  before_action :require_login, :current_user, :current_uri, :path, :current_controller, :current_action
 
   def index
     render template: "welcome/index"
