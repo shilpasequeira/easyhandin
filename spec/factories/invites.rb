d003c1b72c037402ab3a458ff685728d753bdf4e
Shilpa Sequeira <mail@shilpasequeira.com>
2016-03-14 09:40:22 -0700

    Corrects the factories but assignment factory still invalid

--- spec/factories/invites.rb
+++ spec/factories/invites.rb
@@ -1,6 +1,18 @@
 FactoryGirl.define do
   factory :invite do
-    user_role 1
-    email "MyString"
+    user_role :student
+    email Faker::Internet.email
+    university_id Faker::Lorem.characters(10)
+    token Faker::Lorem.characters(10)
+    association :sender, factory: :instructor
+    course
+
+    factory :invite_instructor do
+      user_role :instructor
+    end
+
+    factory :invite_student do
+      user_role :student
+    end
   end
 end
