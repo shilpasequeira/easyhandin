a959e848f4a7a3745ae08302739d0b053c2dc26a
Shilpa Sequeira <mail@shilpasequeira.com>
2016-04-16 01:22:46 -0700

    Refactor of the code

--- app/services/create_branch.rb
+++ app/services/create_branch.rb
@@ -1,17 +1,9 @@
 class CreateBranch < ComposableOperations::Operation
-  processes :client,
-            :org_name,
-            :repo_name,
-            :branch_name
+  processes :org_name, :repo_name, :branch_name
 
   def execute
-
-    # Get ref of first commit
-    ref = client.ref(org_name + "/" + repo_name, "heads/master")
+    ref = Octokit.client.ref(org_name + "/" + repo_name, "heads/master")
     sha = ref[:object][:sha]
-
-    # Create branch
-    client.create_ref(org_name + "/" + repo_name, "heads/" + branch_name, sha)
-
+    Octokit.client.create_ref(org_name + "/" + repo_name, "heads/" + branch_name, sha)
   end
 end
