720d371d5455bb1084e29b9969ede9e9ae6951ff
Shilpa Sequeira <mail@shilpasequeira.com>
2016-04-19 16:53:59 -0700

    Fixes user login as student bug without invite

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
+    user = User.find_by(uid: auth["uid"])
+    invite_token = params[:invite_token]
 
-    if token = request.params[:invite_token]
+    if user.nil? && invite_token.nil? && params[:role] == "student"
+      flash[:error] = "You cannot sign up as a student without an invitation."
+      redirect_to root_url
+      return
+    end
+
+    if invite_token.present?
       invite = Invite.find_by_token(token)
 
       if invite.email != auth["info"]["email"]
@@ -12,13 +20,12 @@ class SessionsController < ApplicationController
         redirect_to root_url
         return
       end
-    elsif request.params[:role] == "student"
-      flash[:error] = "You cannot sign up as a student without an invitation."
-      redirect_to root_url
-      return
     end
 
-    user = User.create_with_omniauth(auth, request.params[:role])
+    if user.nil?
+      user = User.create_with_omniauth(auth, params[:role])
+    end
+
     session[:user_id] = user.id
     session[:access_token] = auth["credentials"]["token"]
 
