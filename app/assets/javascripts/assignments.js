7ca83f595715e92dd66ce9ca50e95f2907c3391f
MonkeySR1 <monkeySR1@outlook.com>
2016-04-18 22:25:40 -0700

    FIX_DATEPICKER: move js to assignment js to be loaded from there

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -1,3 +1,6 @@
+//= require jquery.date-dropdowns.min.js
+//= require jquery.datetimepicker.js
+
 $( document ).ready(function() {
   $('#selectAll').on("change", function() {
     $("#submissions_form input[name='submission_ids[]']").each(function() {
@@ -5,3 +8,22 @@ $( document ).ready(function() {
     });
   });
 });
+
+(function($) {
+    "use strict";
+    //default date & time picker
+    $('#datetimepicker').datetimepicker({
+        dayOfWeekStart: 1,
+        lang: 'en',
+        format:'d/M/Y H:m',
+        defaultDate: new Date()
+    });
+    $('#datetimepicker2').datetimepicker({
+        dayOfWeekStart: 1,
+        lang: 'en',
+        format:'d/M/Y H:m',
+        defaultDate: new Date()
+    });
+})(jQuery);
+
+
